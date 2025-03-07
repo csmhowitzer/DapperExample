96ac19643455278ab89a4426bac9fb06619df91e
csmhowitzer <csmhowitzer@gmail.com>
2025-03-07 13:15:25 -0500

    Added in CORS policy for vue project

--- DapperExample/Program.cs
+++ DapperExample/Program.cs
@@ -1,9 +1,9 @@
-using FluentValidation;
-using DapperExample.Employees;
 using DapperExample.Database;
+using DapperExample.Employees;
+using DapperExample.Projects;
 using DapperExample.Roles;
 using DapperExample.WorkItems;
-using DapperExample.Projects;
+using FluentValidation;
 
 namespace DapperExample;
 
@@ -11,16 +11,27 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        var MyAllowedOrigin = "_myAllowedOrigin";
+
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy(
+                name: MyAllowedOrigin,
+                policy =>
+                {
+                    policy.WithOrigins("http://localhost:5173"); // vue proj
+                }
+            );
+        });
 
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
-        builder.Services.AddSingleton<IDbConnectionFactory>(_ => 
-                new SqliteDbConnectionFactory(
-                    builder.Configuration["DbConnectionString"]!
-                )
-        );
+        builder.Services.AddSingleton<IDbConnectionFactory>(_ => new SqliteDbConnectionFactory(
+            builder.Configuration["DbConnectionString"]!
+        ));
 
         builder.Services.AddSingleton<IEmployeeService, EmployeeService>();
         builder.Services.AddSingleton<IRoleService, RoleService>();
@@ -30,11 +41,13 @@ public class Program
 
         var app = builder.Build();
 
-        if(app.Environment.IsDevelopment())
+        if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
+
+        app.UseCors(MyAllowedOrigin);
 
         app.MapEmployeeEndpoints();
         app.MapRoleEndpoints();
