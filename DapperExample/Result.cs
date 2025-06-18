e961f1de317ff6fab4f6a3d91e7aa0d4875a42d3
csmhowitzer <csmhowitzer@gmail.com>
2025-06-18 09:06:40 -0400

    Adding functional result

--- DapperExample/Result.cs
+++ DapperExample/Result.cs
@@ -35,4 +35,72 @@ public readonly struct Result<TValue, TError>
     }
 }
 
+// Result Extensions
+public static class FunctionalResult
+{
+    public static ProceduralResult<T2, TError> OGMap<T1, T2, TError>(this ProceduralResult<T1, TError> result, Func<T1, T2> map)
+    {
+        if (result.IsSuccess)
+        {
+            T2 mapped = map(result.Value);
+            ProceduralResult<T2, TError> newResult = ProceduralResult<T2, TError>.Success(mapped);
+            return newResult;
+        }
+        return ProceduralResult<T2, TError>.Failure(result.Error);
+    }
 
+    // transform the value into something else, assumes the map is on the success path
+    public static ProceduralResult<T2, TError> Map<T1, T2, TError>(this ProceduralResult<T1, TError> result, Func<T1, T2> map) =>
+        result.IsSuccess
+            ? ProceduralResult<T2, TError>.Success(map(result.Value))
+            : ProceduralResult<T2, TError>.Failure(result.Error);
+
+    // transform the error into something else, assumes the map is on the failure path
+    public static ProceduralResult<T, TNewError> MapError<T, TError, TNewError>(this ProceduralResult<T, TError> result, Func<TError, TNewError> map) =>
+        result.IsSuccess
+            ? ProceduralResult<T, TNewError>.Success(result.Value)
+            : ProceduralResult<T, TNewError>.Failure(map(result.Error));
+
+    // transform the result into something else and return it
+    public static TResult Match<T, TError, TResult>(this ProceduralResult<T, TError> result, Func<T, TResult> mapValue, Func<TError, TResult> mapError) =>
+        result.IsSuccess
+            ? mapValue(result.Value)
+            : mapError(result.Error);
+
+    // bind the result to another result, produces a new result rather than a value. Similar to Map()
+    public static ProceduralResult<T2, TError> Bind<T1, T2, TError>(this ProceduralResult<T1, TError> result, Func<T1, ProceduralResult<T2, TError>> bind) =>
+        result.IsSuccess
+            ? bind(result.Value)
+            : ProceduralResult<T2, TError>.Failure(result.Error);
+}
+
+public class ProceduralResult<TValue, TError>
+{
+    private TValue? _value;
+    private TError? _error;
+
+    public bool IsSuccess { get; }
+
+    public TValue Value
+    {
+        get => IsSuccess ? _value! : throw new InvalidOperationException("Result not successful");
+        private set => _value = value;
+    }
+
+    public TError Error
+    {
+        get => !IsSuccess ? _error! : throw new InvalidOperationException("Result is successful");
+        private set => _error = value;
+    }
+
+    private ProceduralResult(bool isSuccess, TValue? value, TError? error)
+    {
+        IsSuccess = isSuccess;
+        _value = value;
+        _error = error;
+    }
+
+    public static ProceduralResult<TValue, TError> Success(TValue value) => new(true, value, default);
+    public static ProceduralResult<TValue, TError> Failure(TError error) => new(false, default, error);
+
+}
